fc6957af3930776d7b44c6d842a15ad76fdf15dd
Carnegie <carnegiejunior@gmail.com>
2017-11-29 13:12:00 -0300

    Segunda alteração do projeto.

--- src/main/java/tech/metamaker/api/MyFirstProjectWithSpringApplication.java
+++ src/main/java/tech/metamaker/api/MyFirstProjectWithSpringApplication.java
@@ -8,5 +8,6 @@ public class MyFirstProjectWithSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstProjectWithSpringApplication.class, args);
+		System.out.println("Meu primeiro projeto");
 	}
 }
